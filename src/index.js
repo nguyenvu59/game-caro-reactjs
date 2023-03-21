1313bb93c4de4c41f08cce472cb89ee191d62296
nguyenvu59 <nguyenvu5994@gmail.com>
2023-03-21 17:36:36 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -4,16 +4,44 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
 }
 
 class Board extends React.Component {
@@ -26,8 +54,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O'; this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -39,7 +69,21 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    const winner = calculateWinner(this.state.squares);
+    let status; 
+    if (winner) 
+    { 
+      status = 'Winner: ' + winner; 
+    } else 
+    { 
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); 
+    }
+
+    var player = { score: 1, name: 'Jeff' };
+    player.score = 2;
+    var newPlayer = Object.assign({}, player, { score: 2 });
 
     return (
       <div>
